27d4440065be5af4b88d06c800e6e2462a646dc2
Ed Tewiah <etewiah@hotmail.cim>
2015-09-15 10:14:30 +0200

    Experiment with jsonapi for github

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,4 +1,7 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
+export default DS.JSONSerializer.extend({
+  keyForAttribute: function(key) {
+    return Ember.String.decamelize(key);
+  }
 });
---
+++ app/serializers/user.js
@@ -0,0 +1,28 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  normalizeFindRecordResponse(store, type, payload) {
+    // debugger;
+    return {
+      data: {
+        id: payload.login,
+        type: type.modelName,
+        attributes: {
+          name: payload.name,
+          publicRepos: payload.public_repos
+        },
+
+
+        relationships: {
+          repos: {
+            links: {
+              related: payload.repos_url
+            }
+          }
+        }
+
+
+      }
+    }
+  }
+});
