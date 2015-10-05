cb4523f2c57b5e3e4315afb2d08299fe4f932bcb
Ed Tewiah <etewiah@hotmail.cim>
2015-10-05 13:43:16 +0200

    Add i18n

--- app/components/tabs/general-tab.js
+++ app/components/tabs/general-tab.js
@@ -2,35 +2,71 @@ import Ember from 'ember';
 
 
 export default Ember.Component.extend({
-  alquilerDeTemporadaInputFields: [{
-    labelText: "Temporadas:",
-    tooltipText: "Si el inmueble se alquila por temporadas",
-    fieldName: "temporadas",
-    fieldType: "simpleSelect",
-    fieldDbType: "boolean",
-    options: [{
-      value: 1,
-      titleKey: "Si"
+  characteristicasInputFields: [
+    //this comment tricks prettify ;) 
+    {
+      labelText: "fieldLabels.temporadas",
+      tooltipText: "Si el inmueble se alquila por temporadas",
+      fieldName: "temporadas",
+      fieldType: "simpleSelect",
+      fieldDbType: "boolean",
+      options: [{
+        value: 1,
+        titleKey: "Si"
+      }, {
+        value: 0,
+        titleKey: "No"
+      }]
     }, {
-      value: 0,
-      titleKey: "No"
-    }]
-  }, {
-    labelText: "Precio Temp. Alta",
-    tooltipText: false,
-    fieldName: "precioTa",
-    fieldType: "simpleInput"
-  }, {
-    labelText: "Precio Temp. Media",
-    tooltipText: false,
-    fieldName: "precioTm",
-    fieldType: "simpleInput"
-  }, {
-    labelText: "Precio Temp. Baja",
-    tooltipText: false,
-    fieldName: "precioTb",
-    fieldType: "simpleInput"
-  }, ],
+      labelText: "fieldLabels.precioTa",
+      tooltipText: false,
+      fieldName: "precioTa",
+      fieldType: "simpleInput"
+    }, {
+      labelText: "fieldLabels.precioTm",
+      tooltipText: false,
+      fieldName: "precioTm",
+      fieldType: "simpleInput"
+    }, {
+      labelText: "fieldLabels.precioTb",
+      tooltipText: false,
+      fieldName: "precioTb",
+      fieldType: "simpleInput"
+    }
+  ],
+
+  alquilerDeTemporadaInputFields: [
+    //this comment tricks prettify ;) 
+    {
+      labelText: "fieldLabels.temporadas",
+      tooltipText: "Si el inmueble se alquila por temporadas",
+      fieldName: "temporadas",
+      fieldType: "simpleSelect",
+      fieldDbType: "boolean",
+      options: [{
+        value: 1,
+        titleKey: "Si"
+      }, {
+        value: 0,
+        titleKey: "No"
+      }]
+    }, {
+      labelText: "fieldLabels.precioTa",
+      tooltipText: false,
+      fieldName: "precioTa",
+      fieldType: "simpleInput"
+    }, {
+      labelText: "fieldLabels.precioTm",
+      tooltipText: false,
+      fieldName: "precioTm",
+      fieldType: "simpleInput"
+    }, {
+      labelText: "fieldLabels.precioTb",
+      tooltipText: false,
+      fieldName: "precioTb",
+      fieldType: "simpleInput"
+    }
+  ],
   inputFields: [
     // {
     //   labelText: "Precio Venta",
@@ -39,23 +75,25 @@ export default Ember.Component.extend({
     //   fieldType: "simpleInput"
     // }, 
     {
-      labelText: "Precio Anterior",
+      labelText: "fieldLabels.precioAntiguo",
       tooltipText: "El precio antes de la rebaja",
       fieldName: "precioAntiguo",
       fieldType: "simpleInput"
     }, {
-      labelText: "Precio Alquiler",
+      labelText: "fieldLabels.precioAlquiler",
       tooltipText: "0 si el inmueble sólo es para venta.",
       fieldName: "precioAlquiler",
       fieldType: "simpleInput"
-    }, {
-      labelText: "Tipo:",
-      tooltipText: "",
-      fieldName: "precioAlquiler",
-      fieldType: "simpleSelect",
-      options: ["Apartamento", "Villa"]
-    }, {
-      labelText: "Origen:",
+    }, 
+    // {
+    //   labelText: "Tipo:",
+    //   tooltipText: "",
+    //   fieldName: "precioAlquiler",
+    //   fieldType: "simpleSelect",
+    //   options: ["Apartamento", "Villa"]
+    // }, 
+    {
+      labelText: "fieldLabels.origen",
       tooltipText: "",
       fieldName: "idOrigenPropiedad",
       fieldType: "simpleSelect",
---
+++ app/locales/en/translations.js
@@ -0,0 +1,30 @@
+export default {
+  // "some.translation.key": "Text for some.translation.key",
+
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  "fieldLabels": {
+    "precioAntiguo": "Original Price",
+    "precioAlquiler": "Precio Alquiler",
+    "origen": "Origen",
+    "temporadas": "Temporadas",
+    "precioTa": "Precio Temp. Alta",
+    "precioTm": "Precio Temp. Media",
+    "precioTb": "Precio Temp. Baja",
+
+  },
+  "some.translation.key": "Text for some.translation.key",
+
+  "a": {
+    "nested": {
+      "key": "Text for a.nested.key"
+    }
+  },
+
+  "key.with.interpolation": "Text with {{anInterpolation}}"
+};
