caeea18d795332228ede3a2dcbc49fb34e9d6628
Ed Tewiah <etewiah@hotmail.cim>
2015-11-13 11:30:27 +0100

    Fix up material admin header and sidebar

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,30 @@ import Ember from 'ember';
 // const { Route, inject } = Ember;
 
 export default Ember.Route.extend({
+
+
+  activate: function() {
+    // Ember.$('body').toggleClass("admin")
+    // not sure what the advantage of using Ember.$('body') is
+    $('body').addClass('sw-toggled');
+    $('#tw-switch').prop('checked', true);
+
+    $('body').on('change', '#toggle-width input:checkbox', function() {
+      if ($(this).is(':checked')) {
+        $('body').addClass('toggled sw-toggled');
+        // localStorage.setItem('ma-layout-status', 1);
+      } else {
+        $('body').removeClass('toggled sw-toggled');
+        // localStorage.setItem('ma-layout-status', 0);
+      }
+    });
+
+  },
+  // deactivate: function() {
+  //   debugger;
+  //   Ember.$('body').toggleClass("admin");
+  // },
+
   // translationsFetcher: inject.service(),
 
   // beforeModel: function() {
--- vendor/material-admin-functions.js
+++ vendor/material-admin-functions.js
@@ -2,23 +2,24 @@
 * Layout
 */
 (function(){
-   //Get saved layout type from LocalStorage
-   var layoutStatus = localStorage.getItem('ma-layout-status');
-   if (layoutStatus == 1) {
-       $('body').addClass('sw-toggled');
-       $('#tw-switch').prop('checked', true);
-   }
+   // moved to didinsertelement in application view
+   // //Get saved layout type from LocalStorage
+   // var layoutStatus = localStorage.getItem('ma-layout-status');
+   // if (layoutStatus == 1) {
+   //     $('body').addClass('sw-toggled');
+   //     $('#tw-switch').prop('checked', true);
+   // }
    
-   $('body').on('change', '#toggle-width input:checkbox', function(){
-       if ($(this).is(':checked')) {
-     $('body').addClass('toggled sw-toggled');
-     localStorage.setItem('ma-layout-status', 1);
-       }
-       else {
-     $('body').removeClass('toggled sw-toggled');
-     localStorage.setItem('ma-layout-status', 0);
-       }
-   });
+   // $('body').on('change', '#toggle-width input:checkbox', function(){
+   //     if ($(this).is(':checked')) {
+   //   $('body').addClass('toggled sw-toggled');
+   //   localStorage.setItem('ma-layout-status', 1);
+   //     }
+   //     else {
+   //   $('body').removeClass('toggled sw-toggled');
+   //   localStorage.setItem('ma-layout-status', 0);
+   //     }
+   // });
 })();
 
 
