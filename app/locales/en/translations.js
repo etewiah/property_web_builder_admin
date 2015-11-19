60989ad99126e521c9472402c02b10038648152f
Ed Tewiah <etewiah@hotmail.cim>
2015-11-19 19:28:33 +0100

    Add new-client component

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -24,6 +24,9 @@ export default {
 
   "null": "",
 
+  "newProperty": "Nuevo Propiedad",
+  "newClient": "Nuevo Cliente",
+
   "prompts": {
     "fixErrors": "Please correct the errors below"
   },
