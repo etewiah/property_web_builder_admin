6153fd1d504c19f97de93c923f143eb7e68170dc
Ed Tewiah <etewiah@hotmail.cim>
2015-11-20 12:12:22 +0100

    Enable detection of updated value for dynamic-select component

--- app/components/forms/dynamic-select.js
+++ app/components/forms/dynamic-select.js
@@ -9,7 +9,7 @@ export default Ember.Component.extend({
     // /api/v1/lang/field_keys
     var fieldOptions = [];
 
-    fieldOptionKeys.forEach(function(option){
+    fieldOptionKeys.forEach(function(option) {
       var label = this.get("i18n").t(option).string || "Unknown";
       fieldOptions.push({
         value: option,
@@ -20,7 +20,7 @@ export default Ember.Component.extend({
     var currentValue = this.get("resourceObject." + this.fieldDetails.fieldName) || "";
     // this is the only way I can figure out of setting an empty value for selectpicker
     // var currentOption = fieldOptions.findBy("value", currentValue);
-    // var currentOptionTitle = currentOption ? currentOption.label : "";
+    // var currentOptionValue = currentOption ? currentOption.label : "";
     if (Ember.isEmpty(fieldOptions.findBy("value", currentValue))) {
       fieldOptions.pushObject({
         value: "",
@@ -37,27 +37,45 @@ export default Ember.Component.extend({
     var currentValue = this.get("resourceObject." + this.fieldDetails.fieldName) || "";
     var fieldOptions = this.get("fieldOptions");
     var currentOption = fieldOptions.findBy("value", currentValue);
-    var currentOptionTitle = currentOption ? currentOption.label : "";
+    var currentOptionValue = currentOption ? currentOption.value : "";
 
     var sp = this.$(".selectpicker").selectpicker({
       iconBase: 'fa',
       tickIcon: 'fa-check'
-    }).val(currentOptionTitle);
+    }).val(currentOptionValue);
     sp.on('change', function(evt) {
-
-// TODO - currently using the label of the select option 
-// should really use the value as its a bit cleaner..,
-
+      var fieldName = this.get("fieldDetails.fieldName");
       var selected = evt.target.value;
       // $(this).find("option:selected").val();
       var fieldOptions = this.get("fieldOptions");
-      var selectValue = fieldOptions.findBy("label", selected).value;
-      this.set("resourceObject." + this.fieldDetails.fieldName, selectValue);
+      var selectValue = fieldOptions.findBy("value", selected).value;
+      this.set("resourceObject." + fieldName, selectValue);
+
+      var hasChanged = (this.get("originalValue") !== selectValue);
+
+      this.sendAction("valueChangedAction", {
+        hasErrors: false,
+        hasChanged: hasChanged,
+        fieldName: fieldName
+      });
 
     }.bind(this));
 
     this.$('.selectpicker').selectpicker('refresh');
     // this.set("inputValue", this.get("resourceObject." + this.fieldDetails.fieldName));
   }.on('didInsertElement'),
+
+  setOriginalValue: function() {
+    var inputValue = this.get("resourceObject." + this.fieldDetails.fieldName) || "";
+    // inputValue = inputValue || "";
+    this.set("originalValue", inputValue);
+  }.on('init'),
+  // each time I save to the server, I increment resetTrigger value
+  resetOriginalValue: Ember.observer('resetTrigger', function() {
+    var inputValue = this.get("resourceObject." + this.fieldDetails.fieldName);
+    this.set("originalValue", inputValue);
+  }),
+
+
 
 });
