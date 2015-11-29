2b344dba5342632bd95d0b2c68dd98270d1f4fbe
Ed Tewiah <etewiah@hotmail.cim>
2015-11-29 13:24:21 +0100

    Use droplet plugin to upload user avatar

---
+++ app/components/upload/x-droplet.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend(Droplet, {
+  // url: location.origin + '/upload',
+  actions: {
+    // uploadFiles: function(){
+    //   debugger;
+    // }
+  }
+});
