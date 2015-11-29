0bd534d2e8c55f6dbc1756ddd18affb80c52a672
Ed Tewiah <etewiah@hotmail.cim>
2015-11-29 14:05:59 +0100

    Add property fotos-tab component

---
+++ app/components/tabs-property/fotos-tab.js
@@ -0,0 +1,122 @@
+import Ember from 'ember';
+
+
+export default Ember.Component.extend({
+  situacionLeftInputFields: [
+    //this comment tricks prettify ;) 
+    {
+      labelTextTKey: "fieldLabels.localidad",
+      tooltipTextTKey: false,
+      fieldName: "localidad",
+      fieldType: "simpleInput",
+      inputType: "text",
+      constraints: {
+        inputValue: {
+          length: {
+            minimum: 2,
+            tooShort: "needs to have %{count} characters or more"
+          }
+        }
+      }
+    }, {
+      labelTextTKey: "fieldLabels.zona",
+      tooltipTextTKey: false,
+      fieldName: "zona",
+      fieldType: "simpleInput",
+      inputType: "text",
+      constraints: {
+        inputValue: {
+          length: {
+            minimum: 2,
+            tooShort: "needs to have %{count} characters or more"
+          }
+        }
+      }
+    }, {
+      labelTextTKey: "fieldLabels.codigoPostal",
+      tooltipTextTKey: false,
+      fieldName: "codigoPostal",
+      fieldType: "simpleInput",
+      inputType: "text",
+      constraints: {
+        inputValue: {
+          length: {
+            minimum: 2,
+            tooShort: "needs to have %{count} characters or more"
+          }
+        }
+      }
+    },
+  ],
+  situacionRightInputFields: [
+    //this comment tricks prettify ;) 
+    {
+      labelTextTKey: "fieldLabels.direccion",
+      tooltipTextTKey: false,
+      fieldName: "direccion",
+      fieldType: "simpleInput",
+      inputType: "text",
+      constraints: {
+        inputValue: {
+          length: {
+            minimum: 2,
+            tooShort: "needs to have %{count} characters or more"
+          }
+        }
+      }
+    }, {
+      labelTextTKey: "fieldLabels.direccionReal",
+      tooltipTextTKey: false,
+      fieldName: "direccionReal",
+      fieldType: "simpleInput",
+      inputType: "text",
+      constraints: {
+        inputValue: {
+          length: {
+            minimum: 2,
+            tooShort: "needs to have %{count} characters or more"
+          }
+        }
+      }
+    }, {
+      labelTextTKey: "fieldLabels.direccionMapa",
+      tooltipTextTKey: false,
+      fieldName: "direccionMapa",
+      fieldType: "simpleInput",
+      inputType: "text",
+      constraints: {
+        inputValue: {
+          length: {
+            minimum: 2,
+            tooShort: "needs to have %{count} characters or more"
+          }
+        }
+      }
+    },
+  ],
+  // actions: {
+  //   savePropertyResource: function() {
+  //     var propertyResource = this.get("propertyResource");
+
+  //     // var self = this;
+
+  //     function transitionToPost(propertyResource) {
+  //       // self.transitionToRoute('posts.show', post);
+  //     }
+
+  //     function failure(reason) {
+  //       // handle the error
+  //     }
+
+  //     propertyResource.save().then(transitionToPost).catch(failure);
+
+
+  //   }
+  // },
+
+
+  isActive: function() {
+    return this.activeTabName.toLowerCase() === "fotos";
+  }.property("activeTabName"),
+
+});
