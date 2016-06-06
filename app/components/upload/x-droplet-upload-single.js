ee6e4c17ecf59487fd171f67f4974672d43c319a
Ed Tewiah <etewiah@hotmail.cim>
2016-06-06 19:54:04 +0100

    Add alert as carousel photo is being uploaded

--- app/components/upload/x-droplet-upload-single.js
+++ app/components/upload/x-droplet-upload-single.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
+
+//     $window.Droplet = Mixin.create({
+// droplet component sets mixin on global window object
 export default Ember.Component.extend(Droplet, {
 
   // /**
@@ -18,7 +21,7 @@ export default Ember.Component.extend(Droplet, {
   // *
   //  * @property attributeBindings
   //  * @type {Array}
-   
+
   // attributeBindings: ['disabled', 'name', 'type', 'multiple'],
 
   // /**
@@ -27,18 +30,56 @@ export default Ember.Component.extend(Droplet, {
   //  */
   // type: 'file',
 
-    // url: location.origin + '/upload',
+  // url: location.origin + '/upload',
+  showProg: function() {
+    var uploadStatus = this.get("uploadStatus");
+    if (uploadStatus.uploading) {
+      var uploadingIcon = "<i class='fa fa-spinner fa-spin fa-3x fa-fw'></i><span class='sr-only'>Uploading Photo...</span>";
+      swal({
+        title: "Uploading Photo",
+        text: uploadingIcon,
+        html: true,
+        showConfirmButton: false,
+      });
+    } else {
+      if (uploadStatus.error) {
+        // error always seems to come back empty so doing this shoddy
+        // check for filesize in case that is the culprit
+        var fileBeingUploaded = this.files[0];
+        var fileSize = fileBeingUploaded ? fileBeingUploaded.getFileSize() : 0;
+        var errorMessage = "Please try again";
+        if (fileSize > 999999) {
+          errorMessage = "Please ensure photo is less than 1mb";
+        };
+        swal({
+          title: "Sorry, there has been an error.",
+          text: errorMessage,
+          showConfirmButton: true,
+        }, function() {
+          //       this.set("files", []);
+          // a bit drastic but it seems failed files queue up 
+          // and above does not fix it...
+          location.reload();
+        });
+        // 
+      } else {
+        swal.close();
+      }
+    }
+
+  }.observes("uploadStatus.uploading"),
+  // }.on('init'),
+
   actions: {
-    selectRemoteImages: function(){
-      this.set("showRemoteImagesInput", true);      
+    selectRemoteImages: function() {
+      this.set("showRemoteImagesInput", true);
     },
-    addRemoteImages: function(){
+    addRemoteImages: function() {
       var remoteUrls = this.get("remoteUrls");
       // TODO - parse and validate for list of urls
       this.sendAction("addPhotosFromUrlsAction", remoteUrls);
-    }
+    },
     // uploadFiles: function() {
-    //   debugger;
     // }
   },
 
@@ -54,16 +95,18 @@ export default Ember.Component.extend(Droplet, {
   options: {
     // useArray: true
     // requestMethod: Droplet.METHOD.PUT,
-    uploadImmediately: true
-    // ...
-    //   requestMethod – Changed the request verb from default POST;
-    // maximumSize – Set the maximum size for each individual file;
-    // maximumValidFiles – Amount of valid files permitted to be in the queue;
-    // uploadImmediately – Upload files as they're added to the queue;
-    // includeXFileSize – Whether to include the X-File-Size header for progress;
-    // useArray – Changes the FormData name of the file to either file[] or file;
-    // mimeTypes – List of valid MIME types – can also be changed with mimeTypes method;
-    // requestHeaders – Additional request headers to be sent;
-    // requestPostData – Additional POST data to be sent;
+    uploadImmediately: true,
+    // maximumSize: 200,
+    maximumValidFiles: 1
+      // ...
+      //   requestMethod – Changed the request verb from default POST;
+      // maximumSize – Set the maximum size for each individual file;
+      // maximumValidFiles – Amount of valid files permitted to be in the queue;
+      // uploadImmediately – Upload files as they're added to the queue;
+      // includeXFileSize – Whether to include the X-File-Size header for progress;
+      // useArray – Changes the FormData name of the file to either file[] or file;
+      // mimeTypes – List of valid MIME types – can also be changed with mimeTypes method;
+      // requestHeaders – Additional request headers to be sent;
+      // requestPostData – Additional POST data to be sent;
   }
 });
