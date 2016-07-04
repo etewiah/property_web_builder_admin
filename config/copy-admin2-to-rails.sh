9b4f2aedb662424fb52951c650adf7c17ea80ff2
Ed Tewiah <etewiah@hotmail.cim>
2016-07-04 21:26:24 +0200

    Comments on copy to rails script

--- config/copy-admin2-to-rails.sh
+++ config/copy-admin2-to-rails.sh
@@ -1,5 +1,8 @@
 # chmod 755 copy-admin2-to-rails.sh
 
+# - current as of june 2016
+
+
 # rake inmo:read_in_ember_moodleboard
 # after running above, navigate to 
 # http://mgmt.tee.dev:3000/mgmt/deploys/moodleboard
@@ -10,6 +13,21 @@ rm -r $TARGET_DIR
 mkdir -p $TARGET_DIR
 
 cp -r ../dist/ $TARGET_DIR
+
+
+# before running this file, need to make 
+# sure node -v is 0.12.7
+# and run below:
+# ember build --environment=production
+
+# after running, need to edit this file:
+# /Users/etewiah/Ed/sites-2015-spt/inmo1/app/views/layouts/admin2_client.html.erb
+
+
+
+
+
+
 
 # ASSETT_FILES=../dist/assets
 
--- config/copy-to-rails.sh
+++ config/copy-to-rails.sh
@@ -1,43 +1,47 @@
-# chmod 755 copy-to-rails.sh
-
-# rake inmo:read_in_ember_moodleboard
-# after running above, navigate to 
-# http://mgmt.tee.dev:3000/mgmt/deploys/moodleboard
+# # chmod 755 copy-to-rails.sh
 
 
-TARGET_ASSETS_DIR=../../inmo1/public/fe/client/$(date +%Y-%m-%d)/assets
-mkdir -p $TARGET_ASSETS_DIR
-
-rm -r $TARGET_ASSETS_DIR
-ASSETT_FILES=../dist/assets
-
-echo $ASSETT_FILES
-
-for rf in $ASSETT_FILES
-  do
-    echo $rf
-    cp -r $rf $TARGET_ASSETS_DIR
-  done
+# july 2016 -no longer in use
 
 
-TARGET_FONTS_DIR=../../inmo1/public/fe/client/$(date +%Y-%m-%d)/fonts
-mkdir -p $TARGET_FONTS_DIR
-
-rm -r $TARGET_FONTS_DIR
-FONT_FILES=../dist/fonts
-for ff in $FONT_FILES
-do
-  echo $ff
-  cp -r $ff $TARGET_FONTS_DIR
-done
+# # rake inmo:read_in_ember_moodleboard
+# # after running above, navigate to 
+# # http://mgmt.tee.dev:3000/mgmt/deploys/moodleboard
 
 
-TARGET_IMAGES_DIR=../../inmo1/public/fe/client/$(date +%Y-%m-%d)/img
-mkdir -p $TARGET_IMAGES_DIR
+# TARGET_ASSETS_DIR=../../inmo1/public/fe/client/$(date +%Y-%m-%d)/assets
+# mkdir -p $TARGET_ASSETS_DIR
 
-rm -r $TARGET_IMAGES_DIR
-IMG_FILES=../dist/img
-for imgf in $IMG_FILES
-do
-  cp -r $imgf $TARGET_IMAGES_DIR
-done
+# rm -r $TARGET_ASSETS_DIR
+# ASSETT_FILES=../dist/assets
+
+# echo $ASSETT_FILES
+
+# for rf in $ASSETT_FILES
+#   do
+#     echo $rf
+#     cp -r $rf $TARGET_ASSETS_DIR
+#   done
+
+
+# TARGET_FONTS_DIR=../../inmo1/public/fe/client/$(date +%Y-%m-%d)/fonts
+# mkdir -p $TARGET_FONTS_DIR
+
+# rm -r $TARGET_FONTS_DIR
+# FONT_FILES=../dist/fonts
+# for ff in $FONT_FILES
+# do
+#   echo $ff
+#   cp -r $ff $TARGET_FONTS_DIR
+# done
+
+
+# TARGET_IMAGES_DIR=../../inmo1/public/fe/client/$(date +%Y-%m-%d)/img
+# mkdir -p $TARGET_IMAGES_DIR
+
+# rm -r $TARGET_IMAGES_DIR
+# IMG_FILES=../dist/img
+# for imgf in $IMG_FILES
+# do
+#   cp -r $imgf $TARGET_IMAGES_DIR
+# done
