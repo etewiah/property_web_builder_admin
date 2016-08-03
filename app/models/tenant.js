4487f2ceb3218ae41859f2aa2adc111c52587fe0
Ed Tewiah <etewiah@hotmail.cim>
2016-08-03 15:36:07 +0200

    Fix firefox button bug and set def val for social_media

--- app/models/tenant.js
+++ app/models/tenant.js
@@ -1,8 +1,20 @@
 // currently Tenant is returned along with agency details
 // right now this is just for setting style variables
+// aug 2016 - now also for social_media etc..
 var Tenant = Ember.Object.extend({
+  // https://dockyard.com/blog/2016/02/12/defaults-in-ember
+  // social_media: attr('object', { defaultValue: {} }),
+
+  // http://codebrief.com/2012/03/eight-ember-dot-js-gotchas-with-workarounds/
+  init: function() {
+    this._super();
+    var socialMedia = this.get("social_media") || {};
+    // needed because some tenants ended up with this set to null
+    this.set('social_media', socialMedia);
+  },
+
   save: function(complete, error) {
-    var data = this.getProperties( Object.keys(this) );
+    var data = this.getProperties(Object.keys(this));
     // var data = {
     //   address: TenantProperties
     // };
