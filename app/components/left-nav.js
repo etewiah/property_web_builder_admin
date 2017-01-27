38be6b764070ff15ff07a2f02f0ad7b4412861ab
Ed Tewiah <etewiah@hotmail.cim>
2017-01-27 11:25:28 +0100

    unsuccessful attempt to fix + and - indicators on sub-menu

--- app/components/left-nav.js
+++ app/components/left-nav.js
@@ -3,6 +3,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  click: function(ev) {
+    // attempt to fix + and - indicators on sub-menu
+    // - not successfull...
+    var el = ev.target.parentElement;
+    if (el.classList.contains("sm-toggled")) {
+      el.classList.remove("sm-toggled");
+    } else {
+      el.classList.add("sm-toggled");
+    }
+    el.setAttribute("class", el.classList.toString());
+  },
   tagName: 'li',
   // tagName: 'div',
 
@@ -10,31 +21,22 @@ export default Ember.Component.extend({
    * @property classNames
    * @type {String}
    */
-  // classNames: 'files',
+  classNames: 'sm-toggled',
   // toggleUntouched: true,
-  classNameBindings: ['subMenu', 'active', 'toggled'],
-  toggled: function(){
-    // $(".prop-sub-menu").show();
-    // return false;
-    // if (this.get("tab.tabRoute") === "admin.propiedades") {
-    //   // debugger;
-    //   $(".prop-sub-menu").show();
-    //   return this.get("tab.tabRoute") === this.get("activeTabRoute");
-    // } else{
-    //   return true;
-    // }
+  classNameBindings: ['subMenu', 'active'],
+  // toggled: function(){
 
-    // Toggled true will set +/- to -
-    // as I'm forcing submenu to display block by default
-    // (hardcoded in hbs file)
-    // will return as true whenever I have submenuitems
-    return !!this.get("tab.subMenuItems");
-    // return  (this.get("tab.tabRoute") === "admin.propiedades");
-  }.property("activeTabRoute"),
-  active: function(){
+  //   // Toggled false will set +/- to -
+  //   // as I'm forcing submenu to display block by default
+  //   // (hardcoded in hbs file)
+  //   // will return as false whenever I have submenuitems
+  //   return !this.get("tab.subMenuItems");
+  //   // return  (this.get("tab.tabRoute") === "admin.propiedades");
+  // }.property("activeTabRoute"),
+  active: function() {
     return this.get("tab.tabRoute") === this.get("activeTabRoute");
   }.property("activeTabRoute"),
-  subMenu: function(){
+  subMenu: function() {
     // june 2016 - just adds some styles for submenu
     // return this.get("tab.tabRoute") === "admin.website";
     return !!this.get("tab.subMenuItems");
