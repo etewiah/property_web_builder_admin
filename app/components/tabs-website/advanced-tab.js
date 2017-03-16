cb588342d73407516ff77d98e17adfca9bd23ce1
Ed Tewiah <etewiah@hotmail.cim>
2017-03-16 19:57:37 +0100

    replace template-tab with advanced-tab

--- app/components/tabs-website/advanced-tab.js
+++ app/components/tabs-website/advanced-tab.js
@@ -83,43 +83,6 @@ export default TabWithForm.extend(OnReadyMixin, {
       }
       contentItem.save().then(success).catch(failure);
     }
-  },
-
-  // logoPhoto: function() {
-  //   var contentResources = this.get("contentResources");
-  //   var logoContent = contentResources.findBy("key", "logo");
-  //   var logoPhoto = {};
-  //   if (logoContent) {
-  //     logoPhoto = logoContent.get("photoModels.firstObject") || {};
-  //   }
-  //   return logoPhoto;
-  // }.property("contentResources"),
-
-  // editPhotoEndpoint: function() {
-  //   var contentResources = this.get("contentResources");
-  //   var logoContent = contentResources.findBy("key", "logo");
-  //   var logoPhoto = { id: 0 };
-  //   if (logoContent) {
-  //     // June 2016 - scenario where rerenting did not have content resource
-  //     // for logo on server meant I had to add this check..
-  //     logoPhoto = logoContent.get("photoModels.firstObject") || { id: 0};
-  //   }
-  //   var editPhotoEndpoint = "/api/v1/web_contents/photos/" + logoPhoto.id + "/logo" ;
-  //   return editPhotoEndpoint;
-  // }.property("resourceObject.id"),
-
-  // addPhotoEndpoint: function() {
-  //   var contentResources = this.get("contentResources");
-  //   var logoContent = contentResources.findBy("key", "logo") || {
-  //     id: 0
-  //   };
-  //   var addPhotoEndpoint = "/api/v1/web_contents/" + logoContent.id + "/photo";
-  //   return addPhotoEndpoint;
-  // }.property("resourceObject.id"),
-
-  // -using dynamic components so no longer needed
-  // isVisible: function() {
-  //   return this.get("activeTabfieldName").toLowerCase() === "general";
-  // }.property("activeTabName"),
+  }
 
 });
