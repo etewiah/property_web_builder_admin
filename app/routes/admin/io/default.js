aac248ad39259fd5afb857cc10b08673c272be94
Ed Tewiah <etewiah@hotmail.cim>
2017-04-17 06:17:47 +0200

    Default route for io_properties and themes

--- app/routes/admin/io/default.js
+++ app/routes/admin/io/default.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    this.transitionTo("admin.io.tab", "properties");
+    // this.transitionTo("admin.io.tab", "properties");
+    this.transitionTo("admin.io.properties");
   },
 });
