e609856e10a0216864772a326bc0123fb381db75
Ed Tewiah <etewiah@hotmail.cim>
2017-05-28 22:56:25 +0200

    Fix setting of cms block info

--- app/components/cms/cms-part.js
+++ app/components/cms/cms-part.js
@@ -10,22 +10,36 @@ export default Ember.Component.extend({
       this.set("isEditing", false);
     },
   },
-
+  colClass: "col-sm-3",
   colBlocks: Ember.computed('blocksInfo', {
     get(key) {
       var colBlocks = [];
       var allBlocks = this.get("blocksContent.blocks");
       var blocksInfo = this.get("blocksInfo") || [];
 
+      // go through each item of meta info about a block
+      // which is organized as an array of arrays
       blocksInfo.forEach(function(colsInfo) {
         var rowBlocks = [];
+        // inner array represents columns
         colsInfo.forEach(function(rowsInfo) {
-          var rowBlock = allBlocks.findBy("identifier", rowsInfo.label);
+          // below will only work if a block corresponding to meta info
+          // is found
+          // TODO - find way to create block where it does not currently 
+          // exist on server 
+          var rowBlock = allBlocks.findBy("identifier", rowsInfo.label) || {};
+          rowBlock.info = rowsInfo;
           rowBlocks.push(rowBlock);
         });
         colBlocks.push(rowBlocks);
       });
+      // debugger;
       return colBlocks;
     }
-  })
+  }),
+  addPhotoEndpoint: function() {
+    debugger;
+    var addPhotoEndpoint = "/api/v1/cms-pages/photos/" + "1"+ "/" + "2";
+    return addPhotoEndpoint;
+  }.property("contentResources"),
 });
